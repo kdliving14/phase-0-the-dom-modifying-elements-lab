b7a9064c72ee71acf58003f75847630ac096b093
Karter <kdlmuzixnerd14@gmail.com>
2022-07-03 13:30:54 -0700

    commit tree 9de00f2dc2f2a008fb997d10e7925a981786a4a4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const m = document.querySelector("main");
+m.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader)
+
